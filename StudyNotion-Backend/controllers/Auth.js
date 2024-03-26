2a3d62488d3d56c608d19e0e93f2b547c1dd6cfa
Sharadindu Das <remodas7774@gmail.com>
2024-03-26 23:12:00 +0530

    change password controller added

--- StudyNotion-Backend/controllers/Auth.js
+++ StudyNotion-Backend/controllers/Auth.js
@@ -5,6 +5,8 @@ const otpGenerator = require("otp-generator")
 const User = require("../models/User")
 const Otp = require("../models/Otp")
 const Profile = require("../models/Profile")
+const sendEmail = require("../utils/mailer")
+const { updatedPassword } = require("../mail/updatedPassword")
 
 // SendOTP
 exports.sendOTP = async (req, res) => {
@@ -60,7 +62,7 @@ exports.sendOTP = async (req, res) => {
 }
 
 // Signup
-exports.signUp = async (req, res) => {
+exports.signup = async (req, res) => {
     try {
         // get the data from request body (user)
         const { firstName, lastName, email, password, confirmPassword, accountType, contactNumber, otp } = req.body
@@ -82,8 +84,8 @@ exports.signUp = async (req, res) => {
         }
 
         // check if the user already exists in the db or not
-        const existingUser = await User.findOne({email})
-        if(existingUser){
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
             return res.status(400).json({
                 success: false,
                 message: "User is already registered"
@@ -91,18 +93,18 @@ exports.signUp = async (req, res) => {
         }
 
         // find the most recent OTP
-        const recentOTP = await Otp.find({email}).sort({createdAt: -1}).limit(1)
+        const recentOTP = await Otp.find({ email }).sort({ createdAt: -1 }).limit(1)
         console.log(recentOTP)
 
         // validate the OTP
-        if(recentOTP.length === 0){
+        if (recentOTP.length === 0) {
             return res.status(400).json({
                 success: false,
                 message: "Please try for a new OTP"
             })
         }
 
-        else if(otp !== recentOTP[0].otp){
+        else if (otp !== recentOTP[0].otp) {
             return res.status(400).json({
                 success: false,
                 message: "OTP has expired"
@@ -141,11 +143,148 @@ exports.signUp = async (req, res) => {
         console.log(err);
         return res.status(400).json({
             message: 'Something went wrong while signing up',
-            success: false
+            success: false,
+            error: err.message
         });
     }
 }
 
 // Login
+exports.login = async (req, res) => {
+    try {
+        // get email and password as data from request body
+        const { email, password } = req.body
+
+        // validation of the data
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Please provide the email and password correctly"
+            })
+        }
+
+        // check whether the user exists in the db or not
+        const user = await User.findOne({ email }).populate("additionalDetails")
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        // compare password
+        const comparepassword = await bcrypt.compare(password, user.password)
+        if (!comparepassword) {
+            return res.status(403).json({
+                success: false,
+                message: "Please enter the correct password"
+            })
+        }
+
+        // create a payload
+        const payload = {
+            id: user._id,
+            email: user.email,
+            accountType: user.accountType
+        }
+
+        // generate JWT token
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2h" })
+
+        // remove the password and insert the token in the user object
+        user.token = token
+        user.password = undefined
+
+        // creating cookie and sending response
+        const options = {
+            httpOnly: true,
+            expires: new Date(Date.now() + (3 * 24 * 60 * 60 * 1000))
+        }
+
+        res.cookie("token", token, options).status(200).json({
+            success: true,
+            token,
+            user,
+            message: "User logged in successfully"
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error while Logging in",
+            error: err.message
+        })
+    }
+}
 
 // Change Password
+exports.changePassword = async (req, res) => {
+    try {
+        // get user id from req.user object we passed in the auth middleware
+        const userDetails = await User.findById(req.user.id)
+
+        // get oldPassword, newPassword, confirmNewPassword data from request body
+        const { oldPassword, newPassword, confirmNewPassword } = req.body
+
+        // validation of the data
+        if (!oldPassword || !newPassword || !confirmNewPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Please fill the data properly"
+            })
+        }
+
+        // comparing the old password (from user and from db)
+        const compareOldPassword = await bcrypt.compare(oldPassword, userDetails.password)
+        if (!compareOldPassword) {
+            return res.status(401).json({
+                success: false,
+                message: "The password is incorrect"
+            })
+        }
+
+        // check if newPassword and confirmNewPassword matches or not
+        if (newPassword !== confirmNewPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "The password and confirm password does not match",
+            });
+        }
+
+        // updating the password in db
+        const newEncryptedPassword = await bcrypt.hash(newPassword, 10)
+        const updatedUserDetails = await User.findByIdAndUpdate(
+            { _id: req.user.id },
+            { password: newEncryptedPassword },
+            { new: true }
+        )
+
+        // send mail for updated password
+        try {
+            const mailResponse = await sendEmail(
+                updatedUserDetails.email,
+                "Change of Password from StudyNotion",
+                updatedPassword(updatedUserDetails.email, `${updatedUserDetails.firstName} ${updatedUserDetails.lastName}`)
+            )
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Error while sending updated password email"
+            })
+        }
+
+        // returning response
+        return res.status(200).json({
+            success: true,
+            message: "Password updated successfully"
+        });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Error while changing password!",
+            error: err.message
+        })
+    }
+}
